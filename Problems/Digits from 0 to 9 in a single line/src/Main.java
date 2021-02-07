64a6c603cf3660b53635d048565ed2a73771743c
Travis Peebles <cheadge@gmail.com>
2021-02-06 23:29:26 -0600

    attempt fix

--- Problems/Digits from 0 to 9 in a single line/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        // put your code here
-        for (int i = 9; i > -1; i--) {
-            System.out.print(i + " ");
-        }
-    }
-}
